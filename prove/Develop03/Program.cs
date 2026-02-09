88a1dc30f5fde809c54708d85d5ac6cfa260801c
Arctasi19 <joel.young04@gmail.com>
2026-02-09 15:47:23 +0100

    added the comment to the top because I remembered the instructional inconsistency between Canvas instructions and what happened last time I submitted this.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,3 +1,7 @@
+/* The submission instructions in Canvas say to submit the comment through there in I-Learn, but due to instruction
+from my professor, I will include it here as well. 
+To exceed requirements, I added a randomizer class that allows for a random scripture to be selected each time from a set list of scriptures and their references.*/
+
 using System;
 
 class Program
--- prove/Develop03/ScriptureGenerator.cs
+++ prove/Develop03/ScriptureGenerator.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
-using Microsoft.VisualBasic;
 
 public class ScriptureGenerator
 {
