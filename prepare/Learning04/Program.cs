7facc7af3a53e888902d49ab94b20366c8538693
Arctasi19 <joel.young04@gmail.com>
2026-02-16 14:36:56 +0100

    Completed the inheritance homework, creating two subclasses and one superclass that allow for storage of student and class information as well as assignment information in relevant places.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment firstThing = new MathAssignment("92.5", "3-8", "Harry Potter", "Mathing");
+        Console.WriteLine(firstThing.GetSummary());
+        Console.WriteLine(firstThing.GetHomeworkList());
+        WritingAssignment secondThing = new WritingAssignment("The Great British Baking Show Cookbook", "Hermione Granger", "Writing About Potions");
+        Console.WriteLine(secondThing.GetSummary());
+        Console.WriteLine(secondThing.GetWritingInformation());
     }
 }
